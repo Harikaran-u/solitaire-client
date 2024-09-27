f627eec68de064c6e0b1a43a270457ffe5211d43
Harikaran-U <sanjuhari971@gmail.com>
2024-09-27 14:25:46 +0530

    added extra cards split functionality

--- src/components/GameArea.jsx
+++ src/components/GameArea.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../styles/GameArea.css";
 
 import { useEffect, useState } from "react";
@@ -6,58 +6,64 @@ import { Client } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
 import StartGame from "./StartGame";
 
-const cardKeySet = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+// const cardKeySet = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
 const GameArea = () => {
-  const [client, setClient] = useState(null);
   const [pilesData, setPilesData] = useState([]);
   const [extraCards, setExtraCards] = useState([]);
   const [isStarted, setIsStarted] = useState(false);
-  const [isExtraCardAvailable, setIsExtraCardsAvailable] = useState(true);
+  const clientRef = useRef(null);
 
   useEffect(() => {
     const socket = new SockJS("http://localhost:8080/ws");
     const stompClient = new Client({
       webSocketFactory: () => socket,
+      reconnectDelay: 5000,
       onConnect: () => {
         console.log("Connected to web Socket");
-        stompClient.subscribe("/topic/cardList", (message) => {
-          const pilesData = JSON.parse(message.body);
-          setPilesData(pilesData);
-        });
-        stompClient.subscribe("/topic/extraCards", (message) => {
-          const extraCards = JSON.parse(message.body);
-          setExtraCards(extraCards);
-        });
-        stompClient.subscribe("/topic/updatedCards", (message) => {
-          const updatedPilesData = JSON.parse(message.body);
-          setPilesData(updatedPilesData);
-          console.log("state updated through updated cards subscribe");
-        });
+        if (!clientRef.current) {
+          clientRef.current = stompClient;
+          stompClient.subscribe("/topic/cardList", (message) => {
+            const pilesData = JSON.parse(message.body);
+            setPilesData(pilesData);
+          });
+          stompClient.subscribe("/topic/extraCards", (message) => {
+            const extraCards = JSON.parse(message.body);
+            setExtraCards(extraCards);
+          });
+          // stompClient.subscribe("/topic/updatedCards", (message) => {
+          //   console.log(message.body);
+          // });
+        }
       },
       onStompError: (frame) => {
         console.error("Broker Error: " + frame.headers["message"]);
       },
+      onWebSocketClose: () => {
+        console.log("connection lost");
+      },
+      onReconnect: () => {
+        console.log("WebSocket reconnected.");
+      },
     });
     stompClient.activate();
-    setClient(stompClient);
 
     return () => {
-      if (stompClient) {
+      if (stompClient && stompClient.connected) {
         stompClient.deactivate();
       }
     };
   }, []);
 
   const getPileCards = () => {
-    if (client) {
-      client.publish({ destination: "/app/cards/piles" });
+    if (clientRef.current) {
+      clientRef.current.publish({ destination: "/app/cards/piles" });
     }
   };
 
   const getExtraCards = () => {
-    if (client) {
-      client.publish({ destination: "/app/cards/extra" });
+    if (clientRef.current) {
+      clientRef.current.publish({ destination: "/app/cards/extra" });
     }
   };
 
@@ -69,29 +75,28 @@ const GameArea = () => {
 
   const onClickExtraCards = () => {
     const cardSet = extraCards.slice(0, 10);
-    cardSet.forEach((eachCard) => (eachCard.isFlipped = true));
-    setExtraCards(extraCards.slice(10));
-    setPilesData((prevPiles) => {
-      const updatedPiles = { ...prevPiles };
-      cardKeySet.forEach((eachKey) => {
-        const index = parseInt(eachKey) - 1;
-        if (cardSet[index]) {
-          updatedPiles[eachKey] = [...prevPiles[eachKey], cardSet[index]];
-        }
-      });
-      if (client) {
-        client.publish({
-          destination: "/app/cards/updated",
-          body: JSON.stringify(updatedPiles),
-        });
-      }
-      return updatedPiles;
-    });
+    setExtraCards((prevExtraCards) => prevExtraCards.slice(10));
 
-    if (extraCards.length === 10) {
-      setIsExtraCardsAvailable(false);
-    }
+    setPilesData((prevPiles) => {
+      const newPilesData = {};
+      const keySet = Object.keys(prevPiles);
+      keySet.forEach((key) => {
+        const keyIndex = parseInt(key);
+        newPilesData[key] = [...prevPiles[key], cardSet[keyIndex - 1]];
+      });
+      return newPilesData;
+    });
   };
+
+  // useEffect(() => {
+  //   if (clientRef.current && clientRef.current.connected) {
+  //     console.log("inside request");
+  //     clientRef.current.publish({
+  //       destination: "/app/cards/updated",
+  //       body: JSON.stringify(pilesData),
+  //     });
+  //   }
+  // }, [pilesData]);
 
   return (
     <div className="game-area-container">
@@ -124,14 +129,12 @@ const GameArea = () => {
               </div>
             ))}
           </div>
-          {isExtraCardAvailable && (
-            <div
-              className="extra-cards-main-container"
-              onClick={onClickExtraCards}
-            >
+          {extraCards.length > 0 && (
+            <div className="extra-cards-main-container">
               <img
                 src="https://res.cloudinary.com/diuvnny8c/image/upload/v1727269034/kindpng_1537437_kujhfw.png"
                 className="extra-cards"
+                onClick={onClickExtraCards}
               />
             </div>
           )}
